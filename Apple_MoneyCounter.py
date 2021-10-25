5b5d349ae1886ad3dd9acbf79149a3fcbdc65b11
SeanHarveyOfiangga <seanharveyofiangga@gmail.com>
2021-10-25 20:18:36 +0800

    D:/Programming/Assignment #2/Apple_MoneyCounter.py

--- Apple_MoneyCounter.py
+++ Apple_MoneyCounter.py
@@ -1,7 +1,5 @@
-print(f"\nPrice for an Apple is 20PHP each.")
-
 AmountMoney = int(input("\nEnter the amount of money you have: "))
-ApplePrice = 20
+ApplePrice = int(input("\nEnter how much you will buy the apple: "))
 TotalAppleBuyable = AmountMoney // ApplePrice
 Change = AmountMoney - (ApplePrice * TotalAppleBuyable)
 print(f"\nYou can buy {TotalAppleBuyable} apples and your change is {Change} pesos.")
